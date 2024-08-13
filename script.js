4efed5f8ce27bf0a0a76ca5c00b84d89ba2777ea
carlosrueda13 <82956878+carlosrueda13@users.noreply.github.com>
2024-08-13 10:23:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('contactButton').addEventListener('click', function() {
-    window.open('https://wa.me/3214525798', '_blank');
+    window.open('https://wa.me/+573227885618', '_blank');
 });
